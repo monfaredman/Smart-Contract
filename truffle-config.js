5c00567dac71510c69cfe2a537b662a2123bb9a0
monfaredman <monfaredman@outlook.com>
2024-08-22 16:06:36 +0330

    set config for deploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,24 @@
 module.exports = {
+  // networks: {
+  //   development: {
+  //     host: "127.0.0.1", // Localhost
+  //     port: 7545, // Port used by Ganache GUI
+  //     network_id: "5777", // Match any network id
+  //     // network_id: "*", // Match any network id
+  //   },
+  // },
   networks: {
-    development: {
-      host: "127.0.0.1", // Localhost
-      port: 7545, // Port used by Ganache GUI
-      network_id: "5777", // Match any network id
-      // network_id: "*", // Match any network id
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID`
+        ),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
   },
   compilers: {
