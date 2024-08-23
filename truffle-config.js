d2fc544338bc5ebe7a05e8dbcf97a13eca0e2546
monfaredman <monfaredman@outlook.com>
2024-08-24 01:37:53 +0330

    test deploy on alchemy

--- truffle-config.js
+++ truffle-config.js
@@ -1,31 +1,36 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 require("dotenv").config();
 
-const projectId = process.env.REACT_APP_INFURA_PROJECT_ID;
+const alchemyApiKey = process.env.REACT_APP_ALCHEMY_API_KEY;
 const mnemonic = process.env.REACT_APP_MNEMONIC;
-console.log(projectId, mnemonic);
+console.log(alchemyApiKey, mnemonic);
 module.exports = {
   networks: {
-    // Ethereum Mainnet
-    // mainnet: {
-    //   provider: () =>
-    //     new HDWalletProvider(
-    //       mnemonic,
-    //       `https://mainnet.infura.io/v3/${projectId}`
-    //     ),
-    //   network_id: 1, // Mainnet's id
-    //   gas: 5500000, // Gas limit - adjust as needed
-    //   confirmations: 2, // # of confirmations to wait between deployments
-    //   timeoutBlocks: 200, // # of blocks before a deployment times out
-    //   skipDryRun: true, // Skip dry run before migrations
-    // },
-
-    // // Sepolia Testnet
+    // // Ethereum Mainnet
+    mainnet: {
+      networkCheckTimeout: 1000000, // NB: this option does nothing
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          // `wss://mainnet.infura.io/v3/${projectId}`
+          // `wss://eth-mainnet.alchemyapi.io/v2/${alchemyApiKey}`
+          `wss://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`
+          // `https://eth-mainnet.alchemyapi.io/v2/${alchemyApiKey}`
+        ),
+      network_id: 1, // Mainnet's id
+      gas: 5500000, // Gas limit - adjust as needed
+      confirmations: 2, // # of confirmations to wait between deployments
+      timeoutBlocks: 50000, // # of blocks before a deployment times out
+      skipDryRun: true, // Skip dry run before migrations
+    },
+    // Sepolia Testnet
     // sepolia: {
+    //   networkCheckTimeout: 1000000, // NB: this option does nothing
     //   provider: () =>
     //     new HDWalletProvider(
     //       mnemonic,
-    //       `https://sepolia.infura.io/v3/${projectId}`
+    //       // `https://sepolia.infura.io/v3/${projectId}`
+    //       `https://eth-sepolia.alchemyapi.io/v2/${alchemyApiKey}`
     //     ),
     //   network_id: 11155111, // Sepolia's id
     //   gas: 5500000, // Gas limit - adjust as needed
@@ -33,20 +38,19 @@ module.exports = {
     //   timeoutBlocks: 200, // # of blocks before a deployment times out
     //   skipDryRun: true, // Skip dry run before migrations
     // },
-
     // Holesky Testnet
-    holesky: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://holesky.infura.io/v3/${projectId}`
-        ),
-      network_id: 17000, // Holesky's id
-      gas: 5500000, // Gas limit - adjust as needed
-      confirmations: 2, // # of confirmations to wait between deployments
-      timeoutBlocks: 200, // # of blocks before a deployment times out
-      skipDryRun: true, // Skip dry run before migrations
-    },
+    // holesky: {
+    //   provider: () =>
+    //     new HDWalletProvider(
+    //       mnemonic,
+    //       `https://holesky.infura.io/v3/${projectId}`
+    //     ),
+    //   network_id: 17000, // Holesky's id
+    //   gas: 5500000, // Gas limit - adjust as needed
+    //   confirmations: 2, // # of confirmations to wait between deployments
+    //   timeoutBlocks: 50000, // Increase timeout // # of blocks before a deployment times out
+    //   skipDryRun: true, // Skip dry run before migrations
+    // },
   },
 
   compilers: {
