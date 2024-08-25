d9370fec904b7036281d93e5d5a7df937d8c87f0
Moslem Hoseinpoor Monfared <monfaredman@outlook.com>
2024-08-25 18:06:27 +0000

    update contract pragma solidity version

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.21;
 
 contract UserRegistration {
     struct UserInfo {
@@ -96,5 +96,3 @@ contract UserRegistration {
         emit Withdrawal(admin, amount);
     }
 }
-
-
